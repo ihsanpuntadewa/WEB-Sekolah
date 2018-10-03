48ad192689b1480a20f600b9b9b6067364667b88
Roma Debrian1 <roma_coll04@yahoo.com>
2018-10-03 19:05:35 +0700

    Clear

--- akutansi.php
+++ akutansi.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="zxx">
 <head>
-<title>SMK TERPADUs</title>
+<title>SMK TERPADU</title>
 <!-- Meta tag Keywords -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
